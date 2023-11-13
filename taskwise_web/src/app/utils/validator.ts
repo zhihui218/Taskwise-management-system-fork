424614e560d22c118500d8ee21c26b0e8531d7c1
mouyuan123 <79965226+mouyuan123@users.noreply.github.com>
2023-11-13 23:59:49 +0800

    Update validator.ts

--- taskwise_web/src/app/utils/validator.ts
+++ taskwise_web/src/app/utils/validator.ts
@@ -1,26 +1,6 @@
 import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
 import { ROLE } from "./const";
 
-/* Validators for making sure both passwords match to update the password*/
-export function MatchPassword(): ValidatorFn{
-    return (formGroup: FormGroup): ValidationErrors | null => {
-        const password = formGroup.get('password').value;
-        const confirmPassword = formGroup.get('confirmPassword').value;
-        if(!password || !confirmPassword) return null;
-        if(password.length === 0 || confirmPassword.length === 0) return null;
-        if(password !== confirmPassword){
-            return{
-                // Name of the error
-                InvalidPassword: {
-                    // Contents of the error (JSON Format)
-                    message: 'Passwords doesn\'t match',
-                }
-            }
-        }
-        return null;
-    }
-}
-
 /* Validators for making `joining_date` required if registering "ENGINEER || MANAGER" user */
 export function isJoiningDateRequired(): ValidatorFn{
     return (formGroup: FormGroup): ValidationErrors | null => {
