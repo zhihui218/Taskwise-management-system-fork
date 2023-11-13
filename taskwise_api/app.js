6b4b9188be24eac5c4acdc72b034a72433406869
mouyuan123 <79965226+mouyuan123@users.noreply.github.com>
2023-11-13 23:12:06 +0800

    Update app.js

--- taskwise_api/app.js
+++ taskwise_api/app.js
@@ -22,7 +22,6 @@ server.listen(port, () => { console.log(`Server listening on port: ${port}`) });
 const { job } = require('./utils/job-schedule');
 
 var corsOptions = { origin: true };
-const { isAuthorized } = require('./utils/middleware');
 
 
 // Middleware
@@ -34,14 +33,14 @@ app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 // All API Endpoints (Mount '/prefix' for different type of routes)
 //? app.use('/task', isAuthorized, require('./routes/task.route')); => Every request enters `./routes/task.route` will always have '/task' attached to the front of the request url
-app.use('/project', isAuthorized, require('./routes/project.route'));
-app.use('/attachment', isAuthorized, require('./routes/attachment.route'));
-app.use('/task', isAuthorized, require('./routes/task.route'));
+app.use('/project', require('./routes/project.route'));
+app.use('/attachment', require('./routes/attachment.route'));
+app.use('/task', require('./routes/task.route'));
 app.use('/user', require('./routes/user.route'));
-app.use('/ticket', isAuthorized, require('./routes/ticket.route'));
-app.use('/notification', isAuthorized, require('./routes/notification.route'));
-app.use('/dashboard', isAuthorized, require('./routes/dashboard.route'));
-app.use('/chat', isAuthorized, require('./routes/chat.route'));
+app.use('/ticket', irequire('./routes/ticket.route'));
+app.use('/notification', require('./routes/notification.route'));
+app.use('/dashboard', require('./routes/dashboard.route'));
+app.use('/chat', require('./routes/chat.route'));
 
 //* "Object" to be returned when "required()" is invoked
 module.exports = app;
