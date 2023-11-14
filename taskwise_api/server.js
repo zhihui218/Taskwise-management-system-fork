4f6cb21c6714d2caf1c37e452cc8831e4f143c5c
mouyuan123 <mouyuancheng2@gmail.com>
2023-11-14 21:12:43 +0800

    2. Set Up API for Real-Time Feature

--- taskwise_api/server.js
+++ taskwise_api/server.js
@@ -12,19 +12,32 @@ const addNewUser = (username, user_id, socket_id) => {
         online_user.push(
             { 
                 name: username, 
-                _id: user_id, 
-                socket_id: socket_id,
+                _id: user_id,
+                //? To deal with same users accessing their accounts in multiple tabs / browsers
+                socket_id: [socket_id],
                 chatRoom: '',
             }
         )
+    }
+    else {
+        // If the user is already online, just add the new socket_id to the existing user's array of socket IDs.
+        const existingUser = getUser(user_id);
+        if (existingUser && !existingUser.socket_id.includes(socket_id)) existingUser.socket_id.push(socket_id);
     }
 }
 
 //? Remove an user when he / she is offline
 const removeUser = (socket_id) => {
-    const removedUser = online_user.find(user => user.socket_id == socket_id);
-    online_user = online_user.filter(user => user.socket_id !== socket_id);
-    return removedUser; 
+    let removedUser = null;
+    online_user.forEach(user => {
+        if (user.socket_id.includes(socket_id)) {
+            user.socket_id = user.socket_id.filter(id => id !== socket_id);
+            if (user.socket_id.length === 0) removedUser = user;
+        }
+    })
+    // Now remove any users that no longer have any sockets connected.
+    online_user = online_user.filter(user => user.socket_id.length > 0);
+    return removedUser;
 }
 
 const getUser = (user_id) => { return online_user.find(user => user._id == user_id); }
@@ -41,10 +54,8 @@ module.exports = {
                 addNewUser(username, user_id, socket.id);
                 //* Update the current user online status to "true"
                 await UserService.markUserOnlineStatus(user_id, true);
-                //* Tell all clients except of the "Initiator" (user logged in just now) that he/she is online (socket.broadcast.emit())
-                socket.broadcast.emit('userOnline', user_id);
                 //? Broadcast the list of "online" user
-                socket.emit('onlineUser', online_user.map(user => ({ name: user.name, _id: user._id })));
+                io.emit('onlineUser', online_user.map(user => ({ name: user.name, _id: user._id })));
             });
 
             //? Remove the user when his token expired / logout
@@ -53,10 +64,8 @@ module.exports = {
                 if(removedUser){
                     //* Update the logout user online status to "false"
                     await UserService.markUserOnlineStatus(removedUser._id, false);
-                    //* Tell all clients except of the "Initiator" (user logout just now) that he/she is offline (socket.broadcast.emit())
-                    socket.broadcast.emit('userOffline', removedUser._id);
                     //? Broadcast the list of "online" user
-                    socket.emit('onlineUser', online_user.map(user => ({ name: user.name, _id: user._id })));
+                    io.emit('onlineUser', online_user.map(user => ({ name: user.name, _id: user._id })));
                 }
             });
         });
@@ -65,9 +74,12 @@ module.exports = {
 
     sendNotification: function(notification, user_id) {
         if(isUserOnline(user_id)){
-            const formattedNotification = notificationTransform(notification, user_id);
-            //? Real-time emission of the notification to the relevant "ONLINE" user
-            io.to(getUser(user_id).socket_id).emit('notification', formattedNotification);
+            const user = getUser(user_id);
+            if(user.socket_id.length > 0){
+                const formattedNotification = notificationTransform(notification, user_id);
+                //? Real-time emission of the notification to the relevant "ONLINE" user
+                user.socket_id.forEach(socketId => io.to(socketId).emit('notification', formattedNotification));
+            }
         }
     },
 }
