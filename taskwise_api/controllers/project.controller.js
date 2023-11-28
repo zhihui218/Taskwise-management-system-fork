5f6715c3e89f426831d13238b6b579ab25bf2d8f
jylim1001 <ljy_2000@hotmail.com>
2023-11-28 14:04:24 +0800

    Decorate the project-related endpoints with relevant details based on the user's role.

--- taskwise_api/controllers/project.controller.js
+++ taskwise_api/controllers/project.controller.js
@@ -1,9 +1,10 @@
 // Handle requests from user && send response to user
 const projectService = require('../services/project.service');
 const FileController = require('../utils/file.controller');
-const { type, status, notification_type } = require('../constants');
+const { type, status, notification_type, roles } = require('../constants');
 const NotificationController = require('../controllers/notification.controller');
-const middleware = require('../utils/middleware');
+// ADD THIS LINE
+const Decorator = require("../utils/decorator");
 
 exports.createProject = async(req, res, next) => {
     try {
@@ -88,6 +89,7 @@ exports.getProjectsOfClient = async(req, res, next) => {
 }
 
 
+// ADD DECORATORS
 exports.getProjectById = async(req, res, next) => {
 
     try {
@@ -96,15 +98,12 @@ exports.getProjectById = async(req, res, next) => {
 
         const project = await projectService.getProjectById(projectId);
 
-        req.project = project;
+        //? Decorate the project with necessary behaviors
+        let decorated = project.toJSON();
+        decorated = await Decorator.clientDetails(decorated);
+        decorated = await Decorator.getProjectEngineers(decorated);
 
-        //? Go to middleware "getProjectEngineer" => To retrieve list of engineers involved in the project
-        next();
-
-        // res.status(200).json({
-        //     status: true,
-        //     project: project,
-        // })
+        res.status(200).json({ project: decorated })
 
     } catch (error) {
 
@@ -114,32 +113,25 @@ exports.getProjectById = async(req, res, next) => {
     }
 }
 
+// ADD ROLE-BASED ACCESS
 exports.paginateProject = async(req, res, next) => {
 
     try{
         //* Pagination
-        const page = parseInt(req.query.page);
-        const limit = parseInt(req.query.limit);
+        const { page, limit, startIndex, endIndex } = paginationDetails(req);
+        const client_id = req.params.client_id;
 
-        const startIndex = (page - 1)* limit;
-        const endIndex = page * limit;
-
-        //* Check the user roles from the `jwtToken`
-        const user_role = req.role;
-        //* Check whether a user_ id is provided
-        const user_id = req.params.user_id;
         let numOfDocs; let projectPaginate;
-        
-        //* Manager, Engineer, Client viewing their relevant project
-        if(user_role){
-            numOfDocs = await projectService.countDocsNum(user_id, user_role);
-            projectPaginate = await projectService.paginateProjects(user_id, user_role, limit, startIndex);
+
+        if(req.user.role == roles.client || client_id){
+            numOfDocs = await projectService.countClientProjectNum(client_id ? client_id : req.user._id);
+            projectPaginate = await projectService.paginateClientProjects(client_id ? client_id : req.user._id, limit, startIndex);
         }
-        else{
-            //* Manager view client project.
-            numOfDocs = await projectService.countClientDocsNum(user_id);
-            projectPaginate = await projectService.paginateClientProjects(user_id, limit, startIndex);
+        else if(req.user.role == roles.manager){
+            numOfDocs = await projectService.countProjectNum();
+            projectPaginate = await projectService.paginateAllProjects(limit, startIndex);
         }
+        else{ throw new Error("Invalid user or roles"); }
 
         res.status(200).json(
             {
@@ -218,6 +210,7 @@ exports.updateProjectStatus = async(req, res, next) => {
     }
 }
 
+// ADD DECORATORS
 exports.updateProject = async(req, res, next) => {
 
     try {
@@ -235,9 +228,12 @@ exports.updateProject = async(req, res, next) => {
         //* Latest project with "Attachment" updated (Possibly "null" because attachment is OPTIONAL)
         const result = await uploadFiles(files, projectID);
 
+        let decorated = result ? result.toJSON() : latest.toJSON();
+        decorated = await Decorator.clientDetails(decorated);
+
         res.status(200).json({
             status: true,
-            project: result ? result : latest
+            project: decorated
         });
 
     } catch (error) {
@@ -258,26 +254,13 @@ exports.deleteProjectAttachment = async(req, res, next) => {
 
         const attachments = req.body;
 
-        // 1. Delete all the chosen files of a project
-        await FileController.deleteFiles(attachments);
+        const project = await deleteAttachments(projectID, attachments);
 
-        // 2. Generate another array which stores a list of "cloudinary_id" for chosen files
-        const cloudinaryList = attachments.map((attachment) => attachment.cloudinary_id);
-
-        // 3. Delete the "AttachmentSchema" object stored within the project
-        const project = await projectService.deleteProjectAttachment(projectID, cloudinaryList);
-
-
-        res.status(200).json({
-            status: true,
-            project: project
-        });
+        res.status(200).json(project.attachments);
 
     } catch (error) {
 
-        res.status(500).json({
-            status: false,
-        })
+        res.status(500).json(undefined);
 
         return next(error);
     }
@@ -288,10 +271,17 @@ exports.deleteProject = async(req, res, next) => {
     try {
         const projectId = req.params.projectId;
 
+        const project = await projectService.getProjectById(projectId);
+
+        await deleteAttachments(projectId, project.attachments);
+
         //* The folder to be deleted MUST BE "EMPTY"
-        await FileController.deleteFolder(type.project, projectId)
+        await FileController.deleteFolder(type.project, projectId);
+
+        await NotificationController.deleteNotificationByModelId(projectId);
 
         await projectService.deleteProject(projectId);
+
 
         res.status(200).json(true);
 
@@ -312,9 +302,6 @@ exports.getProjectNameAndLeader = async(req, res, next) => {
 
         const project = await projectService.getProjectNameAndLeader(project_id);
 
-        // const leader = await middleware.getProjectLeader(project.selectedLeaderID);
-
-        // res.status(200).json({ name: project.name, leader: leader});
         res.status(200).json({ name: project.name });
 
     } catch (error) {
@@ -343,36 +330,25 @@ exports.getProjectEngineers = async(req, res, next) => {
     }
 }
 
-//! Required by "Dashboard" analysis at the frontend
-exports.getProjectDash = async(req, res, next) => {
-
-    try{
-        // 1. Get all the tasks "GROUP BY" 'Month' from MongoDB
-        const result = await projectService.countProjectByYear();
-        // 2. Calculate the number of "Pending", "On Hold", "Completed" project for "THIS YEAR && THIS MONTH"
-        const { yearObj, monthObj } = transformProjectsByMonthAndYear(result);
-
-        // 3. Get all the tasks "GROUP BY" 'THIS WEEK' from MongoDB
-        const result_2 = await projectService.countProjectByCurrentWeek();
-        // 4. Calculate the number of "Pending", "On Hold", "Completed" project for "THIS WEEK"
-        const weekObj = transformProjectsByWeek(result_2);
-
-        // 5. Send the expected result back to the frontend
-        res.status(200).json({
-            yearResult: yearObj,
-            currentMonthResult: monthObj,
-            currentWeekResult: weekObj
-        });
-
-
-    }catch(error){
-
-        res.status(500).json({});
-
-        return next(new Error());
-    }
+const paginationDetails = (req) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const startIndex = (page - 1)* limit;
+    const endIndex = page * limit;
+    return { page, limit, startIndex, endIndex };
 }
 
+const deleteAttachments = async(project_id, attachments) => {
+    // 1. Delete all the chosen files of a project
+    await FileController.deleteFiles(attachments);
+
+    // 2. Generate another array which stores a list of "cloudinary_id" for chosen files
+    const cloudinaryList = attachments.map((attachment) => attachment.cloudinary_id);
+
+    // 3. Delete the "AttachmentSchema" object stored within the project
+    const project = await projectService.deleteProjectAttachment(project_id, cloudinaryList);
+    return project;
+}
 
 //* Called only when a file is uploaded
 const uploadFiles = async(files, project_id) => {
@@ -384,92 +360,3 @@ const uploadFiles = async(files, project_id) => {
     }
     return undefined;
 }
-
-//* Total number of "Pending", "On Hold", and "Completed" projects in "THIS YEAR && THIS MONTH"
-const transformProjectsByMonthAndYear = (arrayToCount) => {
-    const yearObj = { 
-        [status.pending]: 0,
-        [status.onHold]: 0,
-        [status.completed]: 0,
-        Total: 0
-    };
-    const monthObj = {
-        [status.pending]: 0,
-        [status.onHold]: 0,
-        [status.completed]: 0,
-        Total: 0
-    }
-    
-    // 1. Get the current month
-    const currentMonth = new Date().getMonth() + 1;
-    arrayToCount.forEach(item => {
-        item.statuses.forEach(element => {
-            switch(element.status){
-                case status.pending: 
-                                    yearObj[status.pending] += element.count;
-                                    yearObj['Total'] += element.count;
-                                    if(item._id == currentMonth){
-                                        monthObj[status.pending] += element.count;
-                                        monthObj["Total"] += element.count;
-                                    } 
-                                    break;
-                case status.onHold: 
-                                    yearObj[status.onHold] += element.count;
-                                    yearObj['Total'] += element.count;
-                                    if(item._id == currentMonth){
-                                        monthObj[status.onHold] += element.count;
-                                        monthObj["Total"] += element.count;
-                                    } 
-                                    break;
-                case status.completed: 
-                                    yearObj[status.completed] += element.count;
-                                    yearObj['Total'] += element.count;
-                                    if(item._id == currentMonth){
-                                        monthObj[status.completed] += element.count;
-                                        monthObj["Total"] += element.count;
-                                    }  
-                                    break;
-                default: null;    
-            }
-        })
-    })
-    return { yearObj, monthObj };
-}
-
-//* Total number of "Pending", "On Hold", and "Completed" projects in "THIS YEAR && THIS MONTH"
-const transformProjectsByWeek = (arrayToCount) => {
-    const weekObj = { 
-        [status.pending]: 0,
-        [status.onHold]: 0,
-        [status.completed]: 0,
-        Total: 0
-    };
-
-    arrayToCount.forEach(item => {
-        item.statuses.forEach(element => {
-            switch(element.status){
-                case status.pending: weekObj[status.pending] += element.count; weekObj['Total'] += element.count; break;
-                case status.onHold: weekObj[status.onHold] += element.count; weekObj['Total'] += element.count; break;
-                case status.completed: weekObj[status.completed] += element.count; weekObj['Total'] += element.count; break;
-                default: null;    
-            }
-        })
-    })
-    return weekObj;
-}
-
-/**
- * 
-*       Sample data format:
-        [
-            {
-                "_id": 1,
-                "statuses": [
-                    {
-                        "status": "Pending",
-                        "count": 1
-                    }
-                ]
-            },
-        ]
- */
